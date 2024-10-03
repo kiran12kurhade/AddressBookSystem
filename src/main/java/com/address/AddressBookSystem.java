a948a75c50ecef9df4b0931e009f65f0efecb105
kiran12kuhrade <Github@1999>
2024-10-03 15:25:03 +0530

    [Kiran] Added:Welcome to Address Book System

---
+++ src/main/java/com/address/AddressBookSystem.java
@@ -0,0 +1,12 @@
+package com.address;
+
+
+
+public class AddressBookSystem {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Address Book Program in AddressBookMain class ");
+
+    }
+
+}
